d60335c220f9cd1b37087c89094322b3f6608040
svetlana.panchenko <ivanovasvetlana1991@gmail.com>
2020-07-30 10:31:50 +0300

    [GRAPHQL][server] 1. cleanup

--- graphql/server/src/apollo.config.js
+++ graphql/server/src/apollo.config.js
@@ -4,4 +4,4 @@ module.exports = {
   }
 };
 // command used to publish schema
-// npx  apollo service:push   --localSchemaFile=./schema.graphql --key=service:books-web:293-_f_8dET3-PHt5m50YQ --graph=books-web
+// npx  apollo service:push   --localSchemaFile=./schema.js --key=service:books-web:293-_f_8dET3-PHt5m50YQ --graph=books-web
