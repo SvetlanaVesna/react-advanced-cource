a0de0ca0ade1b4e7695f23af86dfc90bea6ec756
svetlana.panchenko <ivanovasvetlana1991@gmail.com>
2020-09-15 03:06:34 +0300

    [saga] basic test

--- saga/src/sagas/__tests__/basic.test.ts
+++ saga/src/sagas/__tests__/basic.test.ts
@@ -1,11 +1,16 @@
-import { call } from 'redux-saga/effects'
+import { call, put } from 'redux-saga/effects'
 import { getUsers } from '../basic'
 import { api } from '../../utils'
+import * as actionCreators from '../../actionCreators/basic'
 describe('test basic saga', () => {
   test('test call effect', () => {
     const iterator = getUsers()
     expect(JSON.stringify(iterator.next().value)).toEqual(
       JSON.stringify(call(api, 'http://localhost:3000/users?_page=1&limit=20')),
+    )
+    const users: any[] = []
+    expect(JSON.stringify(iterator.next(users).value)).toEqual(
+      JSON.stringify(put(actionCreators.getUsersSuccess(users))),
     )
   })
 })
