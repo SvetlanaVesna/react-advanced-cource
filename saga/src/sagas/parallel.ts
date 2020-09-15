d4ff8eae8e271cb6f65a786e62e44c60916461b6
svetlana.panchenko <ivanovasvetlana1991@gmail.com>
2020-09-15 04:21:42 +0300

    [saga] basic test

--- saga/src/sagas/parallel.ts
+++ saga/src/sagas/parallel.ts
@@ -10,11 +10,10 @@ const API_ENDPOINT_CITIES = 'http://localhost:3000/cities'
 const API_ENDPOINT_USERS = 'http://localhost:3000/users'
 
 function* getData() {
-  const r = yield all([
+  yield all([
     call(fetchResource, API_ENDPOINT_USERS, { type: actionTypes.GET_USERS_SUCCESS }),
     call(fetchResource, API_ENDPOINT_CITIES, { type: actionTypes.GET_CITIES_SUCCESS }),
   ])
-  console.log(r)
 }
 
 function* fetchResource(resource: any, successAction: any) {
