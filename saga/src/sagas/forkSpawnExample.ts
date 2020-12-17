d104ca7461db3bbe7cab6bf921f0ff41c79ccfc1
svetlana.panchenko <ivanovasvetlana1991@gmail.com>
2020-12-18 00:49:51 +0300

    [saga] improve proofs for spawn\fork example

--- saga/src/sagas/forkSpawnExample.ts
+++ saga/src/sagas/forkSpawnExample.ts
@@ -3,31 +3,27 @@ import * as actionTypes from '../actionTypes/forkSpawnExample'
 import { toast } from 'react-toastify'
 import { api } from '../utils'
 
-const API_ENDPOINT_COMMENTS = 'http://localhost:3000/comments'
 const API_ENDPOINT_USERS = 'http://localhost:3000/users'
 
 function* fetchAllWithFork() {
+  console.log('Fork start')
   yield fork(fetchResource, API_ENDPOINT_USERS, {
     type: actionTypes.GET_USERS_SUCCESS,
   })
-  yield fork(fetchResource, API_ENDPOINT_COMMENTS, {
-    type: actionTypes.GET_CITIES_SUCCESS,
-  })
-  yield delay(500)
+  console.log('Fork end')
 }
 
 //spawn runs independently, hence error do not bubble up to parent in this case.
 function* fetchAllWithSpwan() {
+  console.log('Spawn start')
   yield spawn(fetchResource, API_ENDPOINT_USERS, {
     type: actionTypes.GET_USERS_SUCCESS,
   })
-  yield spawn(fetchResource, API_ENDPOINT_COMMENTS, {
-    type: actionTypes.GET_CITIES_SUCCESS,
-  })
-  yield call(delay, 500)
+  console.log('Spawn end')
 }
 
 function* fetchResource(resource: any, successAction: any) {
+  console.log('fetch started')
   try {
     const result = yield call(api, resource)
     yield put({ type: successAction.type, data: result })
@@ -35,22 +31,28 @@ function* fetchResource(resource: any, successAction: any) {
   } catch (e) {
     toast.error('Fail!')
   }
+  yield delay(1000)
+  console.log('fetch finished')
 }
 
 function* mainFork() {
+  console.log('Main start')
   try {
     yield call(fetchAllWithFork)
   } catch (error) {
     yield put({ type: actionTypes.ERROR, error })
   }
+  console.log('Main end')
 }
 
 function* mainSpawn() {
+  console.log('Main start')
   try {
     yield call(fetchAllWithSpwan)
   } catch (error) {
     yield put({ type: actionTypes.ERROR, error })
   }
+  console.log('Main end')
 }
 
 export function* forkSpawnExampleMain() {
